4df9b591d0b9594baeb9da83fb1fe858170413da
xhquan <xhquan3306@163.com>
2024-03-10 11:02:19 +0800

    增加Tcc 分布式事务模式和seata 分布式事务模式

---
+++ order/src/main/java/com/summer/order/service/OrderService.java
@@ -0,0 +1,51 @@
+package com.summer.order.service;
+
+import io.seata.spring.annotation.GlobalTransactional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @Author：xhquan
+ * @Project：cloud-mould
+ * @since：2024/3/10 10:57
+ */
+public class OrderService {
+
+    //====================Tcc 分布式事务====================
+    // 在User服务中，您需要按照TCC模式的流程来调用这些接口
+    @PostMapping("/tryReduceStock")
+    public ResponseEntity<?> tryReduceStock(@RequestBody Object stockOperation) {
+        // 尝试扣减库存，预留资源
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/confirmReduceStock")
+    public ResponseEntity<?> confirmReduceStock(@RequestBody Object stockOperation) {
+        // 确认扣减库存
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/cancelReduceStock")
+    public ResponseEntity<?> cancelReduceStock(@RequestBody Object stockOperation) {
+        // 取消扣减库存，释放资源
+
+        return ResponseEntity.ok().build();
+    }
+
+    //====================seata 分布式事务====================
+    @GlobalTransactional
+    public void createOrder(){
+
+        // 创建订单
+
+        // 调用库存服务
+
+        // 调用指服务
+
+
+    }
+
+}
---
+++ user/src/main/java/com/summer/user/service/TransactionService.java
@@ -0,0 +1,16 @@
+package com.summer.user.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author：xhquan
+ * @Project：cloud-mould
+ * @since：2024/3/10 10:51
+ */
+@Service
+public class TransactionService {
+
+    // ==== 配合Order服务进行TCC模式调用方式 ====
+
+
+}
