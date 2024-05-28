83fb2cca78fbfbb3c8074bc2dc1db498ad1a17cb
pipe <xhquan3306@163.com>
2024-05-28 09:10:19 +0800

    增加网关 集成nacos

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cloud-mould
+辅助工具的搭建
+docker  nacos  安装
+docker run --name nacos-quick -e MODE=standalone -p 8848:8848 -p 9848:9848 -p 9849:9849 -d nacos/nacos-server
+
+docker sentinel 安装
+docker pull bladex/sentinel-dashboard
+docker run --name sentinel \-p 8858:8858 \--privileged=true \--restart=always \-d bladex/sentinel-dashboard
---
+++ gateway/src/main/java/com/pipe/gateway/GatewayServiceApplication.java
@@ -0,0 +1,18 @@
+package com.pipe.gateway;
+
+/**
+ * @Description
+ * @Author pipe
+ * @Date 2024/5/27 16:44
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
+
